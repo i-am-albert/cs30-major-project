8e37480f145ea81b9307ab3b21efbe82e525c7fd
iAmAlbert <57306890+i-am-albert@users.noreply.github.com>
2024-11-26 14:16:00 -0600

    edited proposal, added an asset, started functionality on sketch

--- proposal.md
+++ proposal.md
@@ -5,12 +5,13 @@ Recreation of glap.io, a web based space exploration and spaceship building game
 ## Needs to Have List
 
 - The game's physics, gravity
-- Most of the game's planets, moon, and sun
-- Most of the game's ship modules, with functionality, which includes weight, energy, and thrust
-- Ship builder + ship saving
+- Most of the game's planets, moon, and sun (Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune)
+- Most of the game's ship modules (boosters (normal,eco,hub,landing,super), hubs (normal,power,heart), solar panel, and landing gear), with functionality, which includes weight, energy, and thrust
 
 
 ## Nice to Have List
 
 - Game parity
 - Seeing other ships in multiplayer
+- Ship builder + ship saving
+- Shop to buy special modules
--- sketch.js
+++ sketch.js
@@ -5,9 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// image variables
 let moduleImages;
 let planetImages;
 
+// state variables
+let started = false;
+
+// player variables
+let x;
+let y;
+let xpos;
+let ypos;
+
+class Module {
+  constructor(posx,posy,posx2,posy2,moduleImage) {
+    this.x=x;
+    this.y=y;
+
+  }
+  display() {
+    image(moduleImage,this.x,this.y);
+  }
+}
+
+let moduleArray = []
 
 function preload() {
   moduleImages = {
@@ -15,6 +37,7 @@ function preload() {
     cargo: loadImage("assets/modules/cargo.png"),
     eco_booster: loadImage("assets/modules/eco_booster.png"),
     hub_booster: loadImage("assets/modules/hub_booster.png"),
+    heart_hub: loadImage("assets/modules/heart_hub.png"),
     hub: loadImage("assets/modules/hub.png"),
     landing_booster: loadImage("assets/modules/landing_booster.png"),
     landing_gear: loadImage("assets/modules/landing_gear.png"),
@@ -22,13 +45,34 @@ function preload() {
     solar_panel: loadImage("assets/modules/solar_panel.png"),
     super_booster: loadImage("assets/modules/super_booster.png"),
   };
+  planetImages = {
+    earth: loadImage("assets/planets/earth.png"),
+  };
+}
+
+function displayStartScreen() {
+  fill("white");
+  text("click to start",width/2,height/2);
+}
+
+function displayModules() {
+  for (let module of moduleArray) {
+    module.move();
+    module.display();
+  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  moduleArray.push(new Module(width/2,height/2,moduleImages.heart_hub));
 }
 
 function draw() {
-  background(220);
-  circle(mouseX,mouseY,100);
+  background(0);
+  if (started) {
+    displayModules();
+  }
+  else if (!started) {
+    displayStartScreen();
+  }
 }
