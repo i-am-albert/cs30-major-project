bc3b703f6e4cf369b5108813683c1af68220495a
iAmAlbert <57306890+i-am-albert@users.noreply.github.com>
2024-11-27 14:16:32 -0600

    added ship variables, ship displaying, started on rotation and movement

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// constants
+let MODULE_SIZE = 64;
+
 // image variables
 let moduleImages;
 let planetImages;
@@ -13,23 +16,26 @@ let planetImages;
 let started = false;
 
 // player variables
-let x;
-let y;
-let xpos;
-let ypos;
+let shipx;
+let shipy;
+let shipdx = 0;
+let shipdy = 0;
+let shipRotation = 0;
+let shipdr = 0;
 
 class Module {
-  constructor(posx,posy,posx2,posy2,moduleImage) {
-    this.x=x;
-    this.y=y;
-
+  constructor(posx,posy,posx2,posy2,type) {
+    this.type=type;
+    this.x=posx;
+    this.y=posy;
   }
   display() {
-    image(moduleImage,this.x,this.y);
+    rotate(shipRotation);
+    image(moduleImages.heart_hub,width/2+this.x*MODULE_SIZE,height/2+this.x*MODULE_SIZE,this.x*MODULE_SIZE+MODULE_SIZE,this.x*MODULE_SIZE+MODULE_SIZE);
   }
 }
 
-let moduleArray = []
+let moduleArray = [];
 
 function preload() {
   moduleImages = {
@@ -57,22 +63,44 @@ function displayStartScreen() {
 
 function displayModules() {
   for (let module of moduleArray) {
-    module.move();
-    module.display();
+    // module.move();
+    module.display(module.type,0,0);
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  moduleArray.push(new Module(width/2,height/2,moduleImages.heart_hub));
+  moduleArray.push(new Module(0,0,moduleImages.heart_hub));
 }
 
 function draw() {
   background(0);
   if (started) {
     displayModules();
+    shipMovement();
   }
   else if (!started) {
     displayStartScreen();
   }
+}
+
+function mousePressed() {
+  started = true;
+}
+
+function keyPressed() {
+  if (started) {
+    if (key === "a") {
+      shipdr+=0.01;
+    }
+    if (key === "d") {
+      shipdr-=0.01;
+    }
+  }
+}
+
+function shipMovement() {
+  shipx+=shipdx;
+  shipy+=shipdy;
+  shipRotation+=shipdr;
 }
