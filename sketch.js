ca182aed65722a0cb82b533b1eaf8a6e847016e9
iAmAlbert <ytalberta@gmail.com>
2025-01-15 01:26:13 -0600

    added better start screen, added all planets, particles now render before modules, added a background, added earth indicator so you dont get lost in space, side based module adding to ship, a and d turning now fully works

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-// Project Title
-// Your Name
+// Glap.io recreated
+// Albert Auer
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// will add ship connections with constraints
 
 // Matter.js variables
 let engine;
@@ -26,6 +25,7 @@ let planetImages;
 // state variables
 let started = false;
 
+// booster particles
 class Particle {
   constructor(x, y, type, color, size, rotation, dx, dy, lifetime, angle) {
     this.x = x;
@@ -57,6 +57,7 @@ class Particle {
   }
 }
 
+// general modules
 class Module {
   constructor(posx, posy, type) {
     this.type = type;
@@ -70,7 +71,7 @@ class Module {
     rotate(this.body.angle);
     image(this.type, -MODULE_SIZE/2, -MODULE_SIZE/2, MODULE_SIZE, MODULE_SIZE);
     pop();
-    if (this.body.type.attatched) {
+    if (this.body.type.attached) {
       this.module.body.collisionFilter.category=1;
     }
   }
@@ -83,6 +84,7 @@ class Module {
   }
 }
 
+// booster specific module
 class Booster extends Module {
   constructor(posx, posy, type, thrust) {
     super(posx, posy, type);
@@ -95,7 +97,7 @@ class Booster extends Module {
 
   boost() {
     super.boost();
-    if (this.type.attatched) {
+    if (this.type.attached) {
       let dx = this.thrust*FORCE*Math.cos(this.body.angle - HALF_PI);
       let dy = this.thrust*FORCE*Math.sin(this.body.angle - HALF_PI);
       Matter.Body.applyForce(this.body, this.body.position, {x: dx, y: dy});
@@ -130,17 +132,44 @@ function preload() {
     mars: loadImage("assets/planets/mars.svg"),
     jupiter: loadImage("assets/planets/jupiter.svg"),
     saturn: loadImage("assets/planets/saturn.svg"),
+    uranus: loadImage("assets/planets/uranus.svg"),
     neptune: loadImage("assets/planets/neptune.svg"),
     pluto: loadImage("assets/planets/pluto.svg"),
   };
 }
 
 function displayStartScreen() {
-  fill("white");
-  text("click to start",width/2,height/2);
+  background(0);
+
+  textAlign(CENTER, CENTER);
+  textSize(48);
+  fill(255);
+  text("Glap.io Recreation", width/2, height/5);
+  image(moduleImages.heart_hub, width/2 - 100, height/2 - 75, 200, 200)
+
+  textSize(24);
+  text("WASD to move, drag modules to attach to your ship", width/2, height/3);
+
+  fill(0, 200, 0);
+
+  rect(width/2 - 100, height * 3/4 - 30, 200, 60, 10);
+
+  fill(255);
+  textSize(28);
+  text("Start Game", width/2, height * 3/4);
+}
+
+
+
+function drawPlanet(body, planetImage) {
+  push();
+  translate(body.position.x, body.position.y);
+  image(planetImage, -body.circleRadius, -body.circleRadius, body.circleRadius * 2, body.circleRadius * 2);
+  pop();
 }
 
 function displayModules() {
+  // draw heart module
   Matter.Engine.update(engine);
   translate(width/2 - shipBody.position.x, height/2 - shipBody.position.y);
   push();
@@ -148,34 +177,44 @@ function displayModules() {
   rotate(shipBody.angle);
   image(moduleImages.heart_hub, -MODULE_SIZE/2, -MODULE_SIZE/2, MODULE_SIZE, MODULE_SIZE);
   pop();
-  push();
-  translate(earthBody.position.x, earthBody.position.y);
-  image(planetImages.earth, -300, -300, 600, 600);
-  pop();
-  push();
-  translate(moonBody.position.x, moonBody.position.y);
-  image(planetImages.moon, -150, -150, 300, 300);
-  pop();
-  push();
-  translate(mercuryBody.position.x, mercuryBody.position.y);
-  image(planetImages.mercury, -150, -150, 300, 300);
-  pop();
+
+  // draw planets
+  drawPlanet(earthBody, planetImages.earth);
+  drawPlanet(moonBody, planetImages.moon);
+  drawPlanet(mercuryBody, planetImages.mercury);
+  drawPlanet(venusBody, planetImages.venus);
+  drawPlanet(marsBody, planetImages.mars);
+  drawPlanet(jupiterBody, planetImages.jupiter);
+  drawPlanet(saturnBody, planetImages.saturn);
+  drawPlanet(uranusBody, planetImages.uranus);
+  drawPlanet(neptuneBody, planetImages.neptune);
+  drawPlanet(uranusBody, planetImages.uranus);
+  drawPlanet(plutoBody, planetImages.pluto);
+
+  // draw modules
   for (let module of moduleArray) {
     module.display(module.type,0,0);
   }
 }
 
 function displayParticles() {
+  push();
+  translate((width / 2 - shipBody.position.x), (height / 2 - shipBody.position.y));
+
   for (let particle of particleArray) {
     particle.display();
     particle.move();
     if (particle.lifetime - particle.age < 0) {
-      particleArray.splice(particle, 1);
+      particleArray.splice(particleArray.indexOf(particle), 1);
     }
   }
+
+  pop();
 }
 
+
 function setup() {
+  // basic setup
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
@@ -186,27 +225,28 @@ function setup() {
   Composite = Matter.Composite,
   Constraint = Matter.Constraint,
 
+  // hardcode all planets
   shipBody = Matter.Bodies.rectangle(width / 2, height / 2, MODULE_SIZE, MODULE_SIZE, {frictionAir: 0.0});
   Matter.World.add(world, shipBody);
   earthBody = Matter.Bodies.circle(width / 2, height / 2 - 200, 300, {isStatic: true});
   Matter.World.add(world, earthBody);
-  moonBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  moonBody = Matter.Bodies.circle(0, 0, 250, {isStatic: true});
   Matter.World.add(world, moonBody);
   mercuryBody = Matter.Bodies.circle(0, 500, 150, {isStatic: true});
   Matter.World.add(world, mercuryBody);
-  venusBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  venusBody = Matter.Bodies.circle(0, 750, 150, {isStatic: true});
   Matter.World.add(world, venusBody);
-  marsBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  marsBody = Matter.Bodies.circle(0, 1000, 150, {isStatic: true});
   Matter.World.add(world, marsBody);
-  jupiterBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  jupiterBody = Matter.Bodies.circle(0, 1250, 150, {isStatic: true});
   Matter.World.add(world, jupiterBody);
-  saturnBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  saturnBody = Matter.Bodies.circle(0, 1500, 150, {isStatic: true});
   Matter.World.add(world, saturnBody);
-  uranusBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  uranusBody = Matter.Bodies.circle(0, 1750, 150, {isStatic: true});
   Matter.World.add(world, uranusBody);
-  neptuneBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  neptuneBody = Matter.Bodies.circle(0, 2000, 150, {isStatic: true});
   Matter.World.add(world, neptuneBody);
-  plutoBody = Matter.Bodies.circle(0, 0, 150, {isStatic: true});
+  plutoBody = Matter.Bodies.circle(0, 2250, 150, {isStatic: true});
   Matter.World.add(world, plutoBody);
 
   // example test modules
@@ -223,18 +263,65 @@ function setup() {
 }
 
 function draw() {
-  image(backgroundImage,0,0,width,height);
+  image(backgroundImage, 0, 0, width, height);
 
   if (started) {
+    displayParticles();
     displayModules();
     shipControls();
-    displayParticles();
     moduleDragging();
-  } 
-  else {
+    drawEarthIndicator();
+  } else {
     displayStartScreen();
   }
 }
+
+function drawEarthIndicator() {
+
+  // distance to earth
+  const dx = earthBody.position.x - shipBody.position.x;
+  const dy = earthBody.position.y - shipBody.position.y;
+  const distance = Math.sqrt(dx*dx + dy*dy);
+
+  // indicator position
+  const angleToEarth = atan2(dy, dx);
+  const maxIndicatorRadius = 200;
+  const indicatorX = maxIndicatorRadius*cos(angleToEarth);
+  const indicatorY = maxIndicatorRadius*sin(angleToEarth);
+
+  // indicator size
+  const minSize = 50;
+  const maxSize = 200;
+  const maxDistance = 20000;
+  const indicatorSize = map(distance, 0, maxDistance, maxSize, minSize);
+  const constraintedSize = constrain(indicatorSize, minSize, maxSize);
+
+  // background of cross
+
+  push();
+  translate(shipBody.position.x, shipBody.position.y);
+  noStroke();
+  fill(200, 100);
+  ellipse(indicatorX, indicatorY, constraintedSize, constraintedSize);
+
+  stroke(0, 150);
+  strokeWeight(constraintedSize/10);
+  noFill();
+  ellipse(indicatorX, indicatorY, constraintedSize*0.8, constraintedSize*0.8);
+
+  const crossSize = constraintedSize*0.8;
+  // cross
+  line(
+    indicatorX - crossSize/2, indicatorY, indicatorX + crossSize/2, indicatorY
+  );
+  line(
+    indicatorX, indicatorY - crossSize/2, indicatorX, indicatorY + crossSize/2
+  );
+  pop();
+}
+
+
+
 
 function mousePressed() {
   if (!started) {
@@ -248,99 +335,184 @@ function mousePressed() {
   }
 }
 
+function getModuleConnections(body) {
+  // find connection points for the module
+
+  const cos = Math.cos(body.angle);
+  const sin = Math.sin(body.angle);
+  
+  return [
+    {
+      x: body.position.x + MODULE_SIZE*cos,
+      y: body.position.y + MODULE_SIZE*sin,
+      angle: body.angle - HALF_PI,
+      side: 'right'
+    },
+    {
+      x: body.position.x - MODULE_SIZE*cos,
+      y: body.position.y - MODULE_SIZE*sin,
+      angle: body.angle + HALF_PI,
+      side: 'left'
+    },
+    {
+      x: body.position.x - MODULE_SIZE*sin,
+      y: body.position.y + MODULE_SIZE*cos,
+      angle: body.angle,
+      side: 'top'
+    },
+    {
+      x: body.position.x + MODULE_SIZE*sin,
+      y: body.position.y - MODULE_SIZE*cos,
+      angle: body.angle + PI,
+      side: 'bottom'
+    }
+  ];
+}
+
 function moduleDragging() {
-  if (draggedModule && !draggedModule.type.attatched) {
-    draggedModule.body.collisionFilter.category=0;
+  if (draggedModule && !draggedModule.type.attached) {
+    // no collision with other modules
+    draggedModule.body.collisionFilter.category = 0;
+    
+    // drag module to mouse position
     Matter.Body.setPosition(draggedModule.body, {
       x: mouseX - (width/2 - shipBody.position.x),
       y: mouseY - (height/2 - shipBody.position.y)
     });
+
+    // set variables for checking closest module
     let closestDist = Infinity;
     let closestModule = null;
+    let closestSide = null;
 
+
+    // Check heart module connections
+    let heartConnections = getModuleConnections(shipBody);
+    for (let connection of heartConnections) {
+      let xDist = draggedModule.body.position.x - connection.x;
+      let yDist = draggedModule.body.position.y - connection.y;
+      let distance = Math.sqrt(xDist*xDist + yDist*yDist);
+      
+      if (distance < closestDist) {
+        closestDist = distance;
+        closestModule = shipBody;
+        closestSide = connection;
+      }
+    }
+
+    // Check other module connections
     for (let module of moduleArray) {
-      let xDist = abs(draggedModule.body.position.x-module.body.position.x);
-      let yDist = abs(draggedModule.body.position.y-module.body.position.y);
-      let xDistHeart = abs(draggedModule.body.position.x-shipBody.position.x);
-      let yDistHeart = abs(draggedModule.body.position.y-shipBody.position.y);
-
-      let heartDistance = xDistHeart + yDistHeart;
-      let moduleDistance = xDist + yDist;
-
-      if (heartDistance < moduleDistance) {
-        currentDistance = heartDistance;
-      }
-      else {
-        currentDistance = moduleDistance;
-      }
-
-      console.log(closestModule);
-
-      if (currentDistance < closestDist) {
-        closestDist = currentDistance;
-        if (heartDistance < moduleDistance && module.body !== draggedModule) {
-          closestModule = module.body;
-        }
-        else {
-          closestModule = shipBody;
+      if (module.body !== draggedModule.body && module.type.attached) {
+        let connections = getModuleConnections(module.body);
+        
+        for (let connection of connections) {
+          let xDist = draggedModule.body.position.x - connection.x;
+          let yDist = draggedModule.body.position.y - connection.y;
+          let distance = Math.sqrt(xDist * xDist + yDist * yDist);
+          
+          if (distance < closestDist) {
+            closestDist = distance;
+            closestModule = module.body;
+            closestSide = connection;
+          }
         }
       }
     }
-    draggedModule.body.angle = Math.atan2(closestModule.position.y-draggedModule.body.position.y,closestModule.position.x-draggedModule.body.position.x)+1.6;
-  }
-  else if (draggedModule) {
-    for (constraint of world.constraints) {
-      if (constraint.bodyB === draggedModule.body) {
-        Matter.Composite.remove(engine.world,draggedModule);
-      }
-    } 
+
+    // Snap to closest side if close enough
+    if (closestDist < MODULE_SIZE/2) {
+      Matter.Body.setPosition(draggedModule.body, {
+        x: closestSide.x,
+        y: closestSide.y
+      });
+      Matter.Body.setAngle(draggedModule.body, closestSide.angle);
+    }
   }
 }
 
 function mouseReleased() {
   if (draggedModule) {
-    draggedModule.body.collisionFilter.category=1;
-    if (abs(draggedModule.body.position.x-shipBody.position.x) < MODULE_SIZE && abs(draggedModule.body.position.y-shipBody.position.y) < MODULE_SIZE) {
-      let options = {
-        bodyA: shipBody,
-        bodyB: draggedModule.body,
-        pointA: {x: 0, y: 0},
-        pointB: {x: 0, y: 0},
-        length: MODULE_SIZE+1,
-        stiffness: 1,
-      };
-      let constraint = Matter.Constraint.create(options);
-      Matter.World.add(world,constraint);
-      draggedModule.type.attatched = true;
+    // module can now interact with other modules
+    draggedModule.body.collisionFilter.category = 1;
+
+    // check if point is close 
+    function isCloseTo(x1, y1, x2, y2) {
+      return abs(x1 - x2) < MODULE_SIZE/2 && abs(y1 - y2) < MODULE_SIZE/2;
     }
-    else {
+
+    // check heart module connections
+    let heartConnections = getModuleConnections(shipBody);
+    for (let connection of heartConnections) {
+      if (isCloseTo(draggedModule.body.position.x, draggedModule.body.position.y, connection.x, connection.y)) {
+        let options = {
+          bodyA: shipBody,
+          bodyB: draggedModule.body,
+          pointA: {
+            x: connection.x - shipBody.position.x,
+            y: connection.y - shipBody.position.y
+          },
+          pointB: {
+            x: 0,
+            y: 0
+          },
+          length: 1,
+          stiffness: 0.9,
+          damping: 0.5
+        };
+        let constraint = Matter.Constraint.create(options);
+        Matter.World.add(world, constraint);
+        draggedModule.type.attached = true;
+        Matter.Body.setAngle(draggedModule.body, connection.angle);
+      }
+    }
+
+    // Check connection to other modules
+    if (!draggedModule.type.attached) {
       for (let module of moduleArray) {
-        let xDist = abs(draggedModule.body.position.x-module.body.position.x);
-        let yDist = abs(draggedModule.body.position.y-module.body.position.y);
-        if (xDist < MODULE_SIZE && yDist < MODULE_SIZE && module.type.attatched === true) {
-          draggedModule.body.velocity.x = shipBody.velocity.x;
-          draggedModule.body.velocity.y = shipBody.velocity.y;
-          let options = {
-            bodyA: module.body,
-            bodyB: draggedModule.body,
-            pointA: {x: 0, y: 0},
-            pointB: {x: 0, y: 0},
-            length: MODULE_SIZE-1,
-            stiffness: 0.5,
-          };
-          let constraint = Matter.Constraint.create(options);
-          Matter.World.add(world,constraint);
-          draggedModule.type.attatched = true;
+        if (module.type.attached && module.body !== draggedModule.body) {
+          let connections = getModuleConnections(module.body);
+          
+          for (let connection of connections) {
+            if (isCloseTo(draggedModule.body.position.x, draggedModule.body.position.y, connection.x, connection.y)) {
+              draggedModule.body.velocity.x = shipBody.velocity.x;
+              draggedModule.body.velocity.y = shipBody.velocity.y;
+              
+              let options = {
+                bodyA: module.body,
+                bodyB: draggedModule.body,
+                pointA: {
+                  x: connection.x - module.body.position.x,
+                  y: connection.y - module.body.position.y
+                },
+                pointB: {
+                  x: 0,
+                  y: 0
+                },
+                length: 1,
+                stiffness: 0.9,
+                damping: 0.5
+              };
+              let constraint = Matter.Constraint.create(options);
+              Matter.World.add(world, constraint);
+              draggedModule.type.attached = true;
+              Matter.Body.setAngle(draggedModule.body, connection.angle);
+            }
+          }
+          if (draggedModule.type.attached);
         }
       }
     }
+    // set dragged module speed to 0 
+    if (draggedModule.type.attached) {
+      Matter.Body.setSpeed(draggedModule.body, 0);
+      Matter.Body.setAngularSpeed(draggedModule.body, 0);
+    }
   }
-  Matter.Body.setSpeed(draggedModule.body, 0);
-  Matter.Body.setAngularSpeed(draggedModule.body, 0);
   draggedModule = null;
 }
 
 function shipControls() {
+  // controls for heart module
   // W
   if (keyIsDown(87)) {
     Matter.Body.applyForce(shipBody, shipBody.position, {x: FORCE*Math.cos(shipBody.angle - HALF_PI), y: FORCE*Math.sin(shipBody.angle - HALF_PI)});
@@ -358,45 +530,47 @@ function shipControls() {
     Matter.Body.setAngularVelocity(shipBody, shipBody.angularVelocity + TORQUE);
   }
 
+  // controls for boosters
   for (let module of moduleArray) {
-    if (module.type.attatched) {
-      // https://academo.org/demos/rotation-about-point/
-      let newModuleX = module.body.position.x;
-      let newModuleY = module.body.position.y;
-      // console.log(tempModuleX,tempModuleY);
-      let tempModuleX = newModuleX*cos(-module.body.angle)-newModuleY*sin(-module.body.angle);
-      let tempModuleY = newModuleX*cos(-module.body.angle)-newModuleY*sin(-module.body.angle);
-      // console.log(tempModuleX,tempModuleY);
-      // console.log(module.body.angle%(PI*2));
+    if (module.type.attached) {
+
+      let dx = module.body.position.x - shipBody.position.x;
+      let dy = module.body.position.y - shipBody.position.y;
+
+      // rotate to heart ship coordinates
+      let localX = dx * cos(-shipBody.angle) - dy * sin(-shipBody.angle);
+      let localY = dx * sin(-shipBody.angle) + dy * cos(-shipBody.angle);
+
+
       // W
       if (keyIsDown(87)) {
-        // if (0 < sin(module.body.angle) && sin(module.body.angle) < 1) {
-        if (PI/4 >= (module.body.angle-shipBody.angle)%(PI*2) && (module.body.angle-shipBody.angle)%(PI*2) <= PI/2) {
+        if (cos(module.body.angle - shipBody.angle) > cos(PI / 4)) {
           module.boost();
         }
+          
+          
       }
       // A
       if (keyIsDown(65)) {
-        // if (0 < sin(module.body.angle) && sin(module.body.angle) < 1) {
-        // if (0 >= (module.body.angle-shipBody.angle)%(PI*2) && (module.body.angle-shipBody.angle)%(PI*2) <= PI) {
-        //   module.boost();
-        // }
-        Matter.Body.setAngularVelocity(shipBody, shipBody.angularVelocity - TORQUE);
+        if (localX * localY > 0) {
+          module.boost();
+        }
+        
+        
       }
       // S
       if (keyIsDown(83)) {
-        // if (0 < sin(module.body.angle) && sin(module.body.angle) < 1) {
-        if (PI/4 < (module.body.angle-shipBody.angle)%(PI*2) || (module.body.angle-shipBody.angle)%(PI*2) > PI/2) {
+        if (cos(module.body.angle - shipBody.angle) < -cos(PI / 4)) {
           module.boost();
         }
+          
+          
       }
       // D
       if (keyIsDown(68)) {
-        // if (0 < sin(module.body.angle) && sin(module.body.angle) < 1) {
-        // if (0 >= (module.body.angle-shipBody.angle)%(PI*2) && (module.body.angle-shipBody.angle)%(PI*2) <= PI) {
-        //   module.boost();
-        // }
-        Matter.Body.setAngularVelocity(shipBody, shipBody.angularVelocity + TORQUE);
+        if (localX * localY < 0) {
+          module.boost();
+        }
       }
     }
   }
